d2c286c03e2e9003eea61fba45463abf4188f091
Brian Clark <brian@clark.zone>
2019-02-25 12:22:12 -0500

    various enhancements and upgrades

---
+++ get_checksums.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import requests
+import yaml
+import sys
+
+VERSIONS = [
+  '6.6.1',
+  '6.6.0',
+  '6.5.4',
+  '6.5.3',
+  '6.5.2',
+  '6.5.1',
+  '6.5.0',
+  '6.4.3',
+  '6.4.2',
+  '6.4.1',
+  '6.4.0',
+  '6.3.2',
+  '6.3.1',
+  '6.3.0',
+  '6.2.4',
+  '6.2.3',
+  '6.2.2',
+  '6.2.1',
+  '6.2.0',
+  '6.1.4',
+  '6.1.3',
+  '6.1.2',
+  '6.1.1',
+  '6.1.0',
+  '6.0.1',
+  '6.0.0',
+]
+
+def get_checksum(version, type):
+  if type == 'deb':
+    url = "https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-{}-amd64.deb.sha512".format(version)
+  elif type == 'rpm':
+    url = "https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-{}-x86_64.rpm.sha512".format(version)
+  r = requests.get(url)
+  if r.status_code == 200:
+    checksum = r.text.split(" ")[0]
+    return "sha512:" + checksum
+  else:
+    print "failed to get: " + deb_url
+    sys.exit(1)
+
+data = {
+  'filebeat_deb_checksums': {},
+  'filebeat_rpm_checksums': {}
+}
+for v in VERSIONS:
+    data['filebeat_deb_checksums'][v] = get_checksum(v, 'deb')
+    data['filebeat_rpm_checksums'][v] = get_checksum(v, 'rpm')
+
+with open('vars/checksums.yml', 'w') as yaml_file:
+    yaml.safe_dump(data, yaml_file, default_flow_style=False, explicit_start=True)
--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -16,3 +16,27 @@ end
 describe file('/var/log/filebeat') do
   it { should be_directory }
 end
+
+describe command('filebeat test config -c /etc/filebeat/filebeat.yml') do
+  its('exit_status') { should eq 0 }
+end
+
+describe yaml('/etc/filebeat/filebeat.yml') do
+  # verify name, tags, and fields
+  its('name') { should eq 'myhost' }
+  its(['tags', 0]) { should eq 'foobar' }
+  its(['tags', 1]) { should eq 'webservers' }
+  its(%w(fields myglobalkey)) { should eq 'myglobalvalue' }
+  its(%w(fields environment)) { should eq 'test' }
+  # verify filebeat inputs
+  its(['filebeat', 'inputs', 0, 'enabled']) { should eq false }
+  its(['filebeat', 'inputs', 1, 'paths', 0]) { should eq '/var/log/filebeat/*.log' }
+  # verify modules
+  its(['filebeat', 'modules', 0, 'module']) { should eq 'system' }
+  its(['filebeat', 'modules', 0, 'auth', 'enabled']) { should eq true }
+  # verify processors
+  its(['processors', 0, 'drop_event', 'when', 'contains', 'source']) { should eq 'test' }
+  # verify output
+  its(%w(output file filename)) { should eq 'filebeat' }
+  its(%w(output file path)) { should eq '/tmp/filebeat' }
+end
